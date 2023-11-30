854ab180863fb0092b66605b932988a2b3041c4a
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2023-11-30 16:01:18 +0100

    add buildbot deploy dir

---
+++ deploy/prod/init_db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE graphql_django_api;
+CREATE USER graphql_django_api WITH ENCRYPTED PASSWORD 'graphql_django_api';
+GRANT ALL PRIVILEGES ON DATABASE graphql_django_api to graphql_django_api;
