88b93de92f5e299b46bbf50177454885ac57de62
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-03-05 09:31:50 +0100

    update url to point /

--- graphql_api/urls.py
+++ graphql_api/urls.py
@@ -22,5 +22,6 @@ from graphql_api.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    #path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
